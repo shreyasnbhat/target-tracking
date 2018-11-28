e3e07766abe2de42042ed4542c336590dcc6cf22
Shreyas Bhat <shreyasnbhat@gmail.com>
2018-11-28 10:47:17 +0530

    Added exponential smoothing and plotly traces

--- tracker/mod.py
+++ tracker/mod.py
@@ -8,160 +8,181 @@ import argparse
 import time
 import cv2
 import numpy as np
-from tracker.plotter import smooth_plot_2d, smooth_plot_3d
+from tracker.plotter import *
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from depthmap.monodepth_simple import generate_depth_map_frame, params, init
 from tracker.pyimagesearch.centroidtracker import CentroidTracker
+import pickle
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
-                help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-                help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.2,
-                help="minimum probability to filter weak detections")
-ap.add_argument("-f", "--file", required=True,
-                help="path to video")
-ap.add_argument("-d", "--fcap", type=int, default=50,
-                help="Frame cap")
-args = vars(ap.parse_args())
 
-ct = CentroidTracker(maxDisappeared=args["fcap"])
-(H, W) = (None, None)
+def run():
+    ct = CentroidTracker(maxDisappeared=args["fcap"])
+    (H, W) = (None, None)
 
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-           "sofa", "train", "tvmonitor"]
-COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+               "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+               "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+               "sofa", "train", "tvmonitor"]
+    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-# load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+    # load our serialized model from disk
+    print("[INFO] loading model...")
+    net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-# initialize the video stream, allow the cammera sensor to warmup,
-# and initialize the FPS counter
-print("[INFO] starting video stream...")
-vs = cv2.VideoCapture(args["file"])
-time.sleep(2.0)
-fps = FPS().start()
-init()
-framecnt = 0
-depth_map = None
+    # initialize the video stream, allow the cammera sensor to warmup,
+    # and initialize the FPS counter
+    print("[INFO] starting video stream...")
+    vs = cv2.VideoCapture(args["file"])
+    time.sleep(2.0)
+    fps = FPS().start()
+    init()
+    framecnt = 0
+    depth_map = None
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (512, 256))
+    coordinates = defaultdict(list)
 
-coordinates = defaultdict(list)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (512, 256))
 
-# loop over the frames from the video stream
-while True:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
-    _, frame = vs.read()
-    frame = cv2.resize(frame, (512, 256))
+    while True:
+        # grab the frame from the threaded video stream and resize it
+        # to have a maximum width of 400 pixels
+        _, frame = vs.read()
+        frame = cv2.resize(frame, (512, 256))
 
-    if framecnt % 100 == 0:
-        depth_map = generate_depth_map_frame(params, frame, args["file"])
-        print("Depth Map Generated for frame", framecnt)
+        if framecnt % 100 == 0:
+            depth_map = generate_depth_map_frame(params, frame, args["file"])
+            print("Depth Map Generated for frame", framecnt)
 
-    if framecnt == 500:
+        if framecnt == 500:
+            break
+
+        # grab the frame dimensions and convert it to a blob
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+                                     0.007843, (300, 300), 127.5)
+
+        # pass the blob through the network and obtain the detections and
+        # predictions
+        net.setInput(blob)
+        detections = net.forward()
+        rects = []
+
+        # loop over the detections
+        for i in np.arange(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with
+            # the prediction
+            confidence = detections[0, 0, i, 2]
+
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+            if confidence > args["confidence"]:
+                # extract the index of the class label from the
+                # `detections`, then compute the (x, y)-coordinates of
+                # the bounding box for the object
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                rects.append(box.astype("int"))
+
+                # draw the prediction on the frame
+                label = "{}: {:.2f}%".format(CLASSES[idx],
+                                             confidence * 100)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+
+        # update our centroid tracker using the computed set of bounding
+        # box rectangles
+
+        objects = ct.update(rects, depth_map)
+
+        for objectID in ct.history:
+            prev = None
+            for centroid in ct.history[objectID]:
+                cv2.circle(frame, (centroid[0], centroid[1]), 2, ct.colours[objectID])
+                if prev is not None:
+                    cv2.line(frame, (prev[0], prev[1]), (centroid[0], centroid[1]), ct.colours[objectID], 1)
+                prev = centroid
+
+        for (objectID, centroid) in objects.items():
+            # draw both the ID of the object and the centroid of the
+            # object on the output frame
+            scaledX = int(centroid[0] / 512 * 100)
+            scaledY = int(centroid[1] / 256 * 100)
+            coordinates[objectID].append((scaledX, scaledY, ct.depth[objectID][-1]))
+            print(coordinates)
+            text = str(ct.depth[objectID][-1])
+            cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+
+        # show the output frame
+        cv2.imshow("Frame", frame)
+        out.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+        # update the FPS counter
+        fps.update()
+
+        framecnt += 1
+
+    # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    out.release()
+
+    return coordinates
+
+
+def main(args):
+    path = os.path.join(os.getcwd(),args["file"].split('/')[1].split('.')[0] + ".p")
+    if os.path.exists(path):
+        coordinates = pickle.load(open(path, "rb"))
+    else:
+        coordinates = run()
+        pickle.dump(coordinates, open(path, "wb"))
+
+    plot(coordinates)
+
+
+def plot(coordinates):
+    for i in coordinates:
+        xs = []
+        ys = []
+        zs = []
+
+        for x, y, z in coordinates[i]:
+            xs.append(x)
+            ys.append(y)
+            zs.append(z)
+
+        smooth_plot_3d(xs, ys, zs, "plots/3d/track_" + str(i) + ".html")
+        plt.clf()
+        smooth_plot_2d(xs, zs, "plots/2d/track_" + str(i) + ".html")
         break
 
-    # grab the frame dimensions and convert it to a blob
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-                                 0.007843, (300, 300), 127.5)
 
-    # pass the blob through the network and obtain the detections and
-    # predictions
-    net.setInput(blob)
-    detections = net.forward()
-    rects = []
+if __name__ == '__main__':
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--prototxt", required=True,
+                    help="path to Caffe 'deploy' prototxt file")
+    ap.add_argument("-m", "--model", required=True,
+                    help="path to Caffe pre-trained model")
+    ap.add_argument("-c", "--confidence", type=float, default=0.2,
+                    help="minimum probability to filter weak detections")
+    ap.add_argument("-f", "--file", required=True,
+                    help="path to video")
+    ap.add_argument("-d", "--fcap", type=int, default=50,
+                    help="Frame cap")
+    args = vars(ap.parse_args())
 
-    # loop over the detections
-    for i in np.arange(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[0, 0, i, 2]
-
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
-        if confidence > args["confidence"]:
-            # extract the index of the class label from the
-            # `detections`, then compute the (x, y)-coordinates of
-            # the bounding box for the object
-            idx = int(detections[0, 0, i, 1])
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
-            rects.append(box.astype("int"))
-
-            # draw the prediction on the frame
-            label = "{}: {:.2f}%".format(CLASSES[idx],
-                                         confidence * 100)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-
-    # update our centroid tracker using the computed set of bounding
-    # box rectangles
-
-    objects = ct.update(rects, depth_map)
-
-    for objectID in ct.history:
-        prev = None
-        for centroid in ct.history[objectID]:
-            cv2.circle(frame, (centroid[0], centroid[1]), 2, ct.colours[objectID])
-            if prev is not None:
-                cv2.line(frame, (prev[0], prev[1]), (centroid[0], centroid[1]), ct.colours[objectID], 1)
-            prev = centroid
-
-    for (objectID, centroid) in objects.items():
-        # draw both the ID of the object and the centroid of the
-        # object on the output frame
-        scaledX = int(centroid[0] / 512 * 100)
-        scaledY = int(centroid[1] / 256 * 100)
-        coordinates[objectID].append((scaledX, scaledY, ct.depth[objectID][-1]))
-        print(coordinates)
-        text = str(ct.depth[objectID][-1])
-        cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
-
-    # show the output frame
-    cv2.imshow("Frame", frame)
-    out.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-
-    # update the FPS counter
-    fps.update()
-
-    framecnt += 1
-
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-# do a bit of cleanup
-cv2.destroyAllWindows()
-out.release()
-
-for i in coordinates:
-    xs = []
-    ys = []
-    zs = []
-
-    for x, y, z in coordinates[i]:
-        xs.append(x)
-        ys.append(y)
-        zs.append(z)
-
-    smooth_plot_3d(xs, ys, zs, "plots/3d/track_" + str(i) + ".png")
-    plt.clf()
-    smooth_plot_2d(xs, zs, "plots/2d/track_" + str(i) + ".png")
-    break
+    main(args)
--- tracker/plotter.py
+++ tracker/plotter.py
@@ -1,12 +1,28 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.interpolate as si
-from mpl_toolkits.mplot3d import axes3d, Axes3D
+import plotly.graph_objs as go
+import plotly
+
+SMOOTH_FACTOR = 0.05
+
+def exponential_process(x, smooth_factor):
+    if len(x) > 5:
+        x_res = list()
+        x_res.append(x[0])
+        for i in range(1, len((x))):
+            xn = x[i] * smooth_factor + (1 - smooth_factor) * x_res[-1]
+            x_res.append(xn)
+        return x_res
+    else:
+        return x
 
 
 def smooth_plot_2d(x, y, plot_path):
     t = range(len(x))
-    ipl_t = np.linspace(0.0, len(x) - 1, 100)
+    ipl_t = np.linspace(0.0, len(x) - 1, 1000)
+
+    x = exponential_process(x, SMOOTH_FACTOR)
+    y = exponential_process(y, SMOOTH_FACTOR)
 
     x_tup = si.splrep(t, x, k=3)
     y_tup = si.splrep(t, y, k=3)
@@ -20,22 +36,24 @@ def smooth_plot_2d(x, y, plot_path):
     x_i = si.splev(ipl_t, x_list)
     y_i = si.splev(ipl_t, y_list)
 
-    print(x_i)
-    print(y_i)
+    trace2 = go.Scatter(
+        x=x_i,
+        y=y_i
+    )
 
-    # plt.plot(x, y, '-o')
-    plt.plot(x_i, y_i, 'r')
-    plt.xlim([min(x) - 0.3, max(x) + 0.3])
-    plt.ylim([min(y) - 0.3, max(y) + 0.3])
-    plt.xlabel("X Coordinate")
-    plt.ylabel("Depth")
-    plt.savefig(plot_path)
+    data = [trace2]
+    fig = go.Figure(data=data)
+    plotly.offline.plot(fig, filename=plot_path, auto_open=True)
 
 
 def smooth_plot_3d(x, y, z, plot_path):
     t = range(len(x))
 
-    ipl_t = np.linspace(0.0, len(x) - 1, 100)
+    x = exponential_process(x, SMOOTH_FACTOR)
+    y = exponential_process(y, SMOOTH_FACTOR)
+    z = exponential_process(z, SMOOTH_FACTOR)
+
+    ipl_t = np.linspace(0.0, len(x) - 1, 1000)
 
     x_tup = si.splrep(t, x, k=3)
     y_tup = si.splrep(t, y, k=3)
@@ -54,15 +72,67 @@ def smooth_plot_3d(x, y, z, plot_path):
     y_i = si.splev(ipl_t, y_list)
     z_i = si.splev(ipl_t, z_list)
 
-    fig = plt.figure()
-    ax  = Axes3D(fig)
+    trace1 = go.Scatter3d(
+        x=x_i,
+        y=y_i,
+        z=z_i,
+        mode='markers',
+        marker=dict(
+            size=4,
+            color=z,
+            colorscale='Viridis',
+        ),
+        line=dict(
+            color='#1f77b4',
+            width=2
+        )
+    )
 
-    ax.set_xlim3d(0, 100)
-    ax.set_ylim3d(0, 100)
-    ax.set_zlim3d(0, 100)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
 
-    ax.plot(x_i, y_i, z_i, color='b')
-    plt.savefig(plot_path)
+    data = [trace1]
+    layout = go.Layout(
+        title='Object Track',
+        showlegend=True,
+        width=1600,
+        height=1000,
+        scene=dict(
+            xaxis=dict(
+                title='X Coordinate',
+                titlefont=dict(
+                    family='Courier New, monospace',
+                    size=18,
+                    color='#7f7f7f'
+                ),
+                gridcolor='rgb(255, 255, 255)',
+                zerolinecolor='rgb(255, 255, 255)',
+                showbackground=True,
+                backgroundcolor='rgb(230, 230,230)'
+            ),
+            yaxis=dict(
+                title='Y Coordinate',
+                titlefont=dict(
+                    family='Courier New, monospace',
+                    size=18,
+                    color='#7f7f7f'
+                ),
+                gridcolor='rgb(255, 255, 255)',
+                zerolinecolor='rgb(255, 255, 255)',
+                showbackground=True,
+                backgroundcolor='rgb(230, 230,230)'
+            ),
+            zaxis=dict(
+                title='Depth',
+                titlefont=dict(
+                    family='Courier New, monospace',
+                    size=18,
+                    color='#7f7f7f'
+                ),
+                gridcolor='rgb(255, 255, 255)',
+                zerolinecolor='rgb(255, 255, 255)',
+                showbackground=True,
+                backgroundcolor='rgb(230, 230,230)'
+            )
+        ),
+    )
+    fig = go.Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename=plot_path, auto_open=True)
